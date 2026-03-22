4206669347a916e54446bd1ef4d0bd5c89025455
mrsmile-build <mrsmile4569@gmail.com>
2026-03-22 19:18:35 +0100

    add PWA, WhatsApp capture, instant demo prefill

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE = 'chat2cash-v1';
+const ASSETS = ['/chat2cash/', '/chat2cash/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
